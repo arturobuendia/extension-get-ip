9f3bfaaf1685331271beb3ec565a0761f398762f
arturobuendia <arturo-buendia@hotmail.com>
2024-03-02 19:32:02 -0700

    first commit

---
+++ getip.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", async function() {
+    try {
+      const response = await fetch("https://api.ipify.org");
+      const data = await response.text();
+      document.querySelector("#myip").innerText = data;
+      console.log("IP:", data);
+    } catch (error) {
+      console.error('Error al obtener la dirección IP:', error);
+    }
+
+    const copyButton = document.getElementById("copyButton");
+    const ipElement = document.getElementById("myip");
+    copyButton.addEventListener("click", function() {
+        const ipText = ipElement.textContent.trim();
+        navigator.clipboard.writeText(ipText)
+      });
+  });
